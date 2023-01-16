6d2c1661666343eaf19fa6a9b4658518bd000e5e
hannaneh <hannan.mgh@gmail.com>
2023-01-16 14:16:17 +0330

    1

--- src/KDTree.java
+++ src/KDTree.java
@@ -5,6 +5,18 @@ public class KDTree {
         this.k = k;
     }
 
+    public KDTree CreateTree(float[][] point) {
+        KDTree t2 = new KDTree(k);
+        Node root = null;
+        for (int i = 0; i < point.length; i++) {
+            root = t2.insertNode(root, point[i]);
+        }
+        System.out.println(root.left.point[0]);
+        //System.out.println(root.left.point[1]);
+        return t2;
+
+    }
+
     public Node insertNode(Node root, float[] point) {
         return insert(root, point, 0);
 
@@ -22,10 +34,30 @@ public class KDTree {
             root.right = insert(root.right, point, depth + 1);
 
         return root;
-        
 
     }
 
-    
+    public boolean searchNode(Node root, float point[]) {
+        return search(root, point, 0);
+    }
+
+    public boolean search(Node root, float point[], int depth) {
+        if (root == null)
+            return false;
+        if (arePointsSame(root.point, point))
+            return true;
+        int cd = depth % k;
+        if (point[cd] < root.point[cd])
+            return search(root.left, point, depth + 1);
+
+        return search(root.right, point, depth + 1);
+    }
+
+    public boolean arePointsSame(float point1[], float point2[]) {
+        for (int i = 0; i < k; ++i)
+            if (point1[i] != point2[i])
+                return false;
+        return true;
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -20,30 +20,28 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter the number of dimensions and points.");
-        Node root = null;
+
         int k = input.nextInt();
         int n = input.nextInt();
         float[][] arr = new float[n][k];
-
+        KDTree t1 = new KDTree(k);
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < k; j++) {
                 arr[i][j] = input.nextInt();
             }
         }
+        t1 = t1.CreateTree(arr);
 
-        KDTree t1 = new KDTree(k);
 
-        for (int i = 0; i < n; i++) {
-            root = t1.insertNode(root, arr[i]);
-        }
-        System.out.println(root.left.point[0]);
-        System.out.println(root.left.point[1]);
-       // System.out.println(root.left.point[2]);
-        System.out.println("*******");
-        System.out.println(root.right.left.right.point[0]);
-        System.out.println(root.right.left.right.point[1]);
-        //System.out.println(root.right.left.right.point[2]);
+        // for (int i = 0; i < n; i++) {
+        // root = t1.insertNode(root, arr[i]);
+        // }
         
+        // System.out.println(t1.k);
+        // System.out.println("*******");
+        // System.out.println(root.right.left.right.point[0]);
+        // System.out.println(root.right.left.right.point[1]);
+        // System.out.println(root.right.left.right.point[2]);
 
     }
 }
